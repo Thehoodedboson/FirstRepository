62d84cda45abb2249e78fbbf56f064a92c9582be
thehoodedboson <brandonjb528@gmail.com>
2018-11-14 10:42:59 -0500

    I have no idea what I'm doing

---
+++ MyProject/Main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+void sortedList(std::string *array, int numbOfNames)
+{
+	for (int alphabetical = 0; alphabetical < numbOfNames - 1; ++alphabetical)
+	{
+		int thisNumber = alphabetical;
+
+		for (int insideName = thisNumber + 1; insideName < numbOfNames; ++insideName)
+		{
+			if (array[insideName] < array[thisNumber])
+			{
+				thisNumber = insideName;
+			}
+		}
+		std::swap(array[alphabetical], array[thisNumber]);
+	}
+}
+
+int main()
+{
+	std::cout << "How many names do you wish to enter? ";
+	int numbOfNames;
+	std::cin >> numbOfNames;
+
+	std::string *array = new std::string[numbOfNames];
+
+	for (int i = 0; i < numbOfNames; ++i)
+	{
+		std::cout << "Enter name #" << i + 1 << "\n";
+		std::cin >> array[i];
+	}
+
+	std::cout << "\nBefore sort: \n[";
+	for (int placeHolder = 0; placeHolder < numbOfNames; ++placeHolder)
+	{
+		std::cout << array[placeHolder] << ", ";
+	}
+	std::cout << "]";
+	
+	sortedList(array, numbOfNames);
+
+	std::cout << "\n\nAfter list sort: \n[";
+	for (int placeHolder = 0; placeHolder < numbOfNames; ++placeHolder)
+	{
+		std::cout << array[placeHolder] << ", ";
+	}
+	std::cout << "]";
+
+	delete[] array;
+	
+
+	std::cin.ignore();
+	std::cin.get();
+
+	return 0;
+}
